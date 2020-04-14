3ba46d266d3ebdc451fc340ff512b1a5ea0d7009
Stan le Punk <stanislasiii@gmail.com>
2020-04-14 22:17:06 +0200

    add example to sort min max with a pointer list

--- TEMPLATE/function_template_external_file/utils.cpp
+++ TEMPLATE/function_template_external_file/utils.cpp
@@ -5,7 +5,10 @@ template<class V, typename A, typename... T>
 void set_list(std::vector<V,A> &list, T... elem) {
 	list.clear();
 	list.reserve(sizeof...(elem));
-	char dummy[] = { (list.push_back(elem), '0')... }; 
+	char dummy[] = { (list.push_back(elem), '0')... };
+	// need to use the dummy arguments to avoid the the flag -Werror / -Wunused
+	dummy[0] = 0;
+
 }
 
 template<typename T>
