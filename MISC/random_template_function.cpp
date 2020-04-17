167d4df10e107bb55851930a6fd5008b06776a52
Stan le Punk <stanislasiii@gmail.com>
2020-04-17 18:57:31 +0200

    add different kind of random function

--- MISC/random_template_function.cpp
+++ MISC/random_template_function.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <cmath>
 
 float random(float min, float max) {
 	std::random_device seed;
@@ -15,11 +16,12 @@ double random_double(double min, double max) {
 	return gen_rand_double(generator); 
 }
 
-double random_normal(double min, double max) {
+
+bool random_bool() {
 	std::random_device seed;
 	std::default_random_engine generator(seed());
-	std::normal_distribution<double> gen_rand_double(min,max);
-	return gen_rand_double(generator); 
+	std::uniform_int_distribution<int> gen_rand_int(0,1);
+	return gen_rand_int(generator); 
 }
 
 int random_int(int min, int max) {
@@ -29,20 +31,40 @@ int random_int(int min, int max) {
 	return gen_rand_int(generator); 
 }
 
-bool random_bool() {
+
+double random_normal(double min, double max) {
 	std::random_device seed;
 	std::default_random_engine generator(seed());
-	std::uniform_int_distribution<int> gen_rand_int(0,1);
+	std::normal_distribution<double> gen_rand_double(min,max);
+	return gen_rand_double(generator); 
+}
+
+int random_geom(double arg) {
+	std::random_device seed;
+	std::default_random_engine generator(seed());
+	std::geometric_distribution<int> gen_rand_int(std::abs(arg));
 	return gen_rand_int(generator); 
 }
 
+double random_exp(double arg) {
+	std::random_device seed;
+	std::default_random_engine generator(seed());
+	std::exponential_distribution<double> gen_rand_double(std::abs(arg));
+	return gen_rand_double(generator); 
+}
+
+
+
 
 int main() {
-	std::cout << "random(-1,1): " << random(-10,10) << std::endl;
-	std::cout << "random_int(-1,1): " << random_int(-10,10) << std::endl;
+	std::cout << "random(-10,10): " << random(-10,10) << std::endl;
+	std::cout << "random_int(-10,10): " << random_int(-10,10) << std::endl;
+	std::cout << "random_geom(0.3): " << random_geom(0.3) << std::endl;
+	std::cout << "random_exp(0.3): " << random_exp(0.3) << std::endl;
+
 	std::cout << "random_bool(): " << random_bool() << std::endl;
 	std::cout << "random_normal(-1,1): " << random_normal(-1,1) << std::endl;
-	std::cout << "random_double(-1,1): " << random_double(-10,10) << std::endl;
+	std::cout << "random_double(-10,10): " << random_double(-10,10) << std::endl;
 }
 
 
