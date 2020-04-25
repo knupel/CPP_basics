32eba224a62f3d7b9a85c34d909bc475e6f69f53
Stan le Punk <stanislasiii@gmail.com>
2020-04-25 21:58:07 +0200

    improve exemple for type detection

--- MISC/typename_test.cpp
+++ MISC/typename_test.cpp
@@ -3,18 +3,22 @@
 
 template <typename T>
 void test(T arg) {
-	if(std::is_same<T, char>::value) {
-		std::cout << "it's a char" << std::endl;
-	} else if(std::is_same<T, std::string>::value) {
-		std::cout << "it's a std::string" << std::endl;
-	} else if(std::is_same<T, int>::value) {
-		std::cout << "it's a int" << std::endl;
-	} else if(std::is_same<T, float>::value) {
-		std::cout << "it's a float" << std::endl;
-	} else if(std::is_same<T, double>::value) {
-		std::cout << "it's a double" << std::endl;
-	} else if(std::is_same<T, bool>::value) {
-		std::cout << "it's a bool" << std::endl;
+	if(std::is_arithmetic<T>::value) {
+		if(std::is_same<T, char>::value) {
+			std::cout << "it's a char" << std::endl;
+		} else if(std::is_same<T, std::string>::value) {
+			std::cout << "it's a std::string" << std::endl;
+		} else if(std::is_same<T, int>::value) {
+			std::cout << "it's a int" << std::endl;
+		} else if(std::is_same<T, float>::value) {
+			std::cout << "it's a float" << std::endl;
+		} else if(std::is_same<T, double>::value) {
+			std::cout << "it's a double" << std::endl;
+		} else if(std::is_same<T, bool>::value) {
+			std::cout << "it's a bool" << std::endl;
+		} else {
+			std::cout << "c'est un drôle de nombre" << std::endl;
+		}
 	} else {
 		std::cout << "c'est un machin ou peut-être un truc" << std::endl;
 	}
@@ -22,10 +26,12 @@ void test(T arg) {
 
 
 int main() {
+	long double ld = 12;
 	test(1);
 	test('c');
 	test(1.0f);
 	test(1.0);
 	test(true);
+	test(ld);
 	test("ola");
 }
--- test.cpp
+++ test.cpp
@@ -1,73 +1,27 @@
 #include <iostream>
 #include <vector>
 
-template <class T>
-void set_list(std::vector<T> &vl) {
-	vl.push_back(2);
-	std::cout << vl.at(0) << std::endl;
+void test(int a, int b) {
+	std::cout << a <<std::endl;
+	std::cout << b <<std::endl;
 }
 
-template <class T>
 class Mother {
-
 public:
-	std::vector<T> v_list;
-	static int instance;
-	Mother() {
-		std::cout << "Mother template default constructor" << std::endl;
-		instance++;
-	}
+	Mother() {}
+	~Mother() {}
 
-	~Mother() {
-		std::cout << "Mother template destructor" << std::endl;
-		instance--;
-	}
-
-	void what() {
-		std::cout << "MOTHER ahahaha..." << std::endl;
-	}
-	
-	int get_instance() {
-		return instance;
+	void test() {
+		std::cout << "nothing" << std::endl;
+		::test(1,2);
 	}
 };
 
 
-template <class T>
-class Child : public Mother<T> {
-private:
-	T arg;
-public:
-	Child() {
-		std::cout << "Child template default constructor" << std::endl;
-		set_list(this->v_list);
-	}
-
-	Child(T arg) : arg(arg) {
-		std::cout << "Child template parametric constructor" << std::endl;
-		set_list(this->v_list);
-	}
-
-	~Child() {
-		std::cout << "Child template destructor" << std::endl;
-	}
-
-	void what() {
-		std::cout << "CHILD ohhhh..." << std::endl;
-	}
-};
-
-
-template <class T>
-int Mother<T>::instance = 0;
 
 
 int main() {
-	// Mother<int> mother;
-	Child<int> child;
-	Child<int> child2(3);
-	std::cout << "Mother<int>::instance: " <<  Mother<int>::instance << std::endl;
-	std::cout << "Child<int>::instance: " <<  Child<int>::instance << std::endl;
-	child.what();
+	Mother mother;
+	mother.test();
 }
 
