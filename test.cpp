0d3429115a9963e61c17a484a73d076e21e25542
Stan le Punk <stanislasiii@gmail.com>
2020-06-20 14:57:32 +0200

    test

--- test.cpp
+++ test.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
-void test(int a, int b) {
-	std::cout << a <<std::endl;
-	std::cout << b <<std::endl;
+void test() {
+	float a = 11.0;
+	float b = 2.0;
+	// float m = a%b;
+	std::cout << fmod(a,b) << std::endl;
+	double tau = 2.0 *M_PI;
+	float longitude = 2.3f;
+	double theta = fmod(longitude,tau);
+	std::cout << theta << std::endl;
 }
-
-class Mother {
-public:
-	Mother() {}
-	~Mother() {}
-
-	void test() {
-		std::cout << "nothing" << std::endl;
-		::test(1,2);
-	}
-};
-
-
 
 
 int main() {
-	Mother mother;
-	mother.test();
+	test();
 }
 
