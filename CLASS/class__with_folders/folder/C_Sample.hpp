72b8e72b84adc31bdfad4479d5133defb9575e54
Stanislas Marçais / Knupel <stanislasiii@gmail.com>
2022-01-15 17:12:46 +0100

    add test for compile with class in folder

---
+++ CLASS/class__with_folders/folder/C_Sample.hpp
@@ -0,0 +1,12 @@
+#ifndef C_SAMPLE_H
+# define C_SAMPLE_H
+
+class Sample {
+public:
+	int var;
+	Sample();
+	~Sample();
+	void methode(void);
+}; 
+
+#endif
---
+++ CLASS/class__with_folders/main.cpp
@@ -0,0 +1,10 @@
+#include "./folder/C_Sample.hpp"
+#include <iostream>
+
+int main() {
+	Sample s;
+	s.methode();
+	s.var = 42;
+	std::cout << "change Sample s.var in main: " << s.var << std::endl;
+	return 0;
+}
